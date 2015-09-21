2d0a82497d2651cd71a5b469837a6d0a20ed0148
charlotteVDD <charlottevandend@gmail.com>
2015-09-21 15:32:05 +0200

    ex16

---
+++ exercises/016/solution.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Created on Mon Sep 21 10:08:02 2015
+@author: CharlotteVDD
+solution.py that prints "abcdefghijklnmopqrstuvwxyz",
+using the print() function."""
+print("abcdefghijklnmopqrstuvwxyz")
