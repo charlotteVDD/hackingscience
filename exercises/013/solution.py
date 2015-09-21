0734c3ce78b37b4cf5960f0dba6a89403cb8d828
charlotteVDD <charlottevandend@gmail.com>
2015-09-21 14:24:23 +0200

    ex13

---
+++ exercises/013/solution.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Created on Mon Sep 21 10:08:02 2015
+@author: CharlotteVDD
+print the factorial of 27, by using the factorial
+function from the math module"""
+a = 2 + 2
+print(a == 5)
