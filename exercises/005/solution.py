80c8e8a291167d9bd21f8f4984817ed64a1a6f25
charlotteVDD <charlottevandend@gmail.com>
2015-09-21 12:24:07 +0200

    ex5 git add exercises

---
+++ exercises/005/solution.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Created on Mon Sep 21 10:08:02 2015
+@author: CharlotteVDD"""
+"""program that print the reverse alphabet with the vowels capitalised"""
+a = 42
+print(a)
