87ca9a56a3bcb2ca753217314ce8e5f116c07abf
charlotteVDD <charlottevandend@gmail.com>
2015-09-21 15:22:40 +0200

    ex15

---
+++ exercises/015/solution.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Created on Mon Sep 21 10:08:02 2015
+@author: CharlotteVDD
+print the result for 245850922 / 78256779 on one line, and the result of
+ (1 + √5) / 2 on the next."""
+a = 245850922 / 78256779
+b = (1 + 5 ** 0.5)/2
+c = [a, b]
+for i in c:
+    print(i)
