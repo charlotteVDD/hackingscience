fd15288aea5f205d9b19c1ea8204de0f0d539bbf
charlotteVDD <charlottevandend@gmail.com>
2015-09-21 12:16:24 +0200

    corrections ex4

--- exercises/004/solution.py
+++ exercises/004/solution.py
@@ -2,5 +2,4 @@
 """Created on Mon Sep 21 10:08:02 2015
 @author: CharlotteVDD"""
 """program that print the reverse alphabet with the vowels capitalised"""
-
-print("zyxwUtsrqpOnmlkjIhgfEdcbA")
+print("zyxwvUtsrqpOnmlkjIhgfEdcbA")
