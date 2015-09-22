64123c47f15330edf7e7ccdfcf6d58af1aed1061
charlotteVDD <charlottevandend@gmail.com>
2015-09-22 10:26:54 +0200

    ex30

--- exercises/030/solution.py
+++ exercises/030/solution.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Created on Mon Sep 21 10:08:02 2015
 @author: CharlotteVDD
-prints every even numbers between 1 and 100 inclusive, one line per each"""
-for i in range(100):
-    print(i+1)
+prints the sum of every even numbers in the range [0; 100]"""
+n = 0
+for i in range(101):
+    if i % 2 == 0:
+        print(i)
