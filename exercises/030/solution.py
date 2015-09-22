030e0c95fbfd002c25a36eb95def577e0497b2ed
charlotteVDD <charlottevandend@gmail.com>
2015-09-22 09:31:15 +0200

    ex30

--- exercises/030/solution.py
+++ exercises/030/solution.py
@@ -2,5 +2,5 @@
 """Created on Mon Sep 21 10:08:02 2015
 @author: CharlotteVDD
 prints every even numbers between 1 and 100 inclusive, one line per each"""
-for i in range(1, 101):
-    print(i)
+for i in range(100):
+    print(i+1)
