50e65d75fe40ba120d9827f1375d6815c6a0f36d
charlotteVDD <charlottevandend@gmail.com>
2015-09-23 22:57:35 +0200

    ex200

---
+++ exercises/200/solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Created on Sep 23 10:08:02 2015
+@author: CharlotteVDD."""
+
+def is_prime(x):
+    for i in range(2, x):
+        r = x % i
+        if  r == 0:
+            return(False)
+        else:
+            return(True)
+
+print(is_prime(13))
+        
