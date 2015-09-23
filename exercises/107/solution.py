3026d925970aa91d490c893f5ccff7febd8e423a
charlotteVDD <charlottevandend@gmail.com>
2015-09-23 14:51:32 +0200

    ex107

---
+++ exercises/107/solution.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Created on Mon Sep 21 10:08:02 2015
+@author: CharlotteVDD
+."""
+import operator
+
+
+my_class = [['Kermit Wade', 27], ['Hattie Schleusner', 67], ['Ben Ball', 5],
+            ['William Lee', 2]]
+
+
+def select_student(l, m):
+    accepted = []
+    refused = []
+    for i in l:
+        if i[1] >= m:
+            accepted.append(i)
+        else:
+            refused.append(i)
+            get_mark = operator.itemgetter(1)
+            ref = list(sorted(refused, key=get_mark))
+    status = {'Accepted': accepted, 'Refused': ref}
+    return(status)
+
+print(select_student(my_class, 20))
