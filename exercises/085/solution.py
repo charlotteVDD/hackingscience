266a57ff05aa5b333268d200a2f1fdc550d206c7
charlotteVDD <charlottevandend@gmail.com>
2015-09-23 08:48:35 +0200

    ex085

---
+++ exercises/085/solution.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""Created on Mon Sep 21 10:08:02 2015
+@author: CharlotteVDD
+sort_a_list that takes a list as argument and return the list sorted
+in the descending order, such as:
+In [1]: from solution import sort_a_list
+In [2]: l = [1, 3, 2, 4, 6, 5, 9, 7]
+In [3]: sort_a_list(l)
+Out[3]: [9, 7, 6, 5, 4, 3, 2, 1]"""
+import operator
+
+
+def sort_a_list(l):
+    a = sorted(l)
+    a.reverse()
+    return(a)
+
+my_class = [[6, 'Joshua Tran'], [37, 'Jeanette Wafer'], [85, 'Susan Maddox'],
+            [84, 'Joseph Peter'], [12, 'Bonnie Torres'], [36, 'John Freeman'],
+            [27, 'Betty Askins'], [22, 'Mark Osbourne'], [42, 'Lidia Robel']]
+
+
+def sort_by_mark(l):
+    sort_a_list(my_class)
+    for i in my_class:
+        print(i)
+
+sort_by_mark(my_class)
+
+
+getname = operator.itemgetter(1)
+a = list(map(getname, my_class))
+
+b = list(sorted(my_class, key=getname))
+
+
+def getName(l):
+    b = list(sorted(l, key=getname))
+    for i in b:
+        print(i)
+    return(b)
+
+getName(my_class)
+
+
+def get_marks(l):
+    for i in range(len(l)):
+        marks = operator.itemgetter(0)(l[i])
+        print(marks)
+    return(marks)
