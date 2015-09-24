8515ea290cf01d9d061d316b24419fbea7127134
charlotteVDD <charlottevandend@gmail.com>
2015-09-24 12:01:37 +0200

    ex202

---
+++ exercises/202/solution.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 10:02:47 2015
+starts_with(haystack, needle) with haystack and needle of type string and
+returning a boolean.
+starts_with will return True if the string in the haystack parameter
+starts with needle, and False otherwise.
+@author: CharlotteVDD
+"""
+
+
+def starts_with(haystack, needle):
+    for i in range(len(needle)):
+        if needle[i] != haystack[i]:
+            print('a')
+            return False
+        else:
+            continue
+    return True
