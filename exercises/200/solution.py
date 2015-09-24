f704d3e6e7e4e90cb14f33b79a63703bc74b864b
charlotteVDD <charlottevandend@gmail.com>
2015-09-24 10:40:42 +0200

    corr200

--- exercises/200/solution.py
+++ exercises/200/solution.py
@@ -17,9 +17,13 @@ def is_prime_old(x):
 def is_prime(x):
     if x == 1:
         return False
+    elif x == 0:
+        return False
     else:
         for i in range(2, int(x ** 0.5) + 1):
             if x % i == 0:
                 return False
             else:
                 return True
+
+print(is_prime(0)) 
