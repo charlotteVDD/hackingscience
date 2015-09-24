09b35ec8926215d77f0e569a4c99d5eb1231d7a7
charlotteVDD <charlottevandend@gmail.com>
2015-09-24 16:09:40 +0200

    corr210

--- exercises/210/solution.py
+++ exercises/210/solution.py
@@ -7,7 +7,10 @@ import is_prime
 
 liste = range(1000)
 result = 0
+prime = []
 for i in range(1000):
-    is_prime.is_prime(i)
-    result = result + i
-print(i)
+    a = is_prime.is_prime(i)
+    if a == True:
+        result = result + i
+
+print(result)
