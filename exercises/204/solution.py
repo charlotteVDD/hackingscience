4d98fe2ce657aace5274a5cd333a3213228215be
charlotteVDD <charlottevandend@gmail.com>
2015-09-24 15:23:58 +0200

    corr203

--- exercises/204/solution.py
+++ exercises/204/solution.py
@@ -13,18 +13,10 @@ def perfect_shuffle(deck):
     mid = int(n / 2)
     if len(deck) == 2:
         pf = [deck[0], deck[1]]
-    elif len(deck) > 0:
+    elif len(deck) > 2:
         deck1 = list(itertools.islice(deck, mid - 1))
         deck2 = list(itertools.islice(deck, mid, n))
         for i in range(mid - 1):
             pf.append(deck1[i])
             pf.append(deck2[i])
     return(pf)
-
-
-l1 = []
-l2 = [0, 1]
-l3 = range(30)
-print(perfect_shuffle(l1))
-print(perfect_shuffle(l2))
-print(perfect_shuffle(l3))
