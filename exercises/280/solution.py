d2e85698f11ca1773476d2d8091e3d6f14189782
charlotteVDD <charlottevandend@gmail.com>
2015-09-25 16:04:39 +0200

    ex280

---
+++ exercises/280/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 15:14:22 2015
+ex 280
+@author: CharlotteVDD
+"""
+import sys
+
+
+try:
+    print(sys.argv[1])
+except(IndexError):
+    print("Not enough parameter")
