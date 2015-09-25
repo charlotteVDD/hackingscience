47283dc4c7ef75213f72866ae2c9116f7c144eea
charlotteVDD <charlottevandend@gmail.com>
2015-09-25 15:08:29 +0200

    ex260

---
+++ exercises/260/solution.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+"""
+Created on Fri Sep 25 08:58:34 2015
+
+@author: CharlotteVDD
+"""
+import numpy as np
+from math import pow
+from math import sqrt
+
+
+x = [2, 3]
+y = [5, 6]
+
+
+def euclidean(a, b):
+    result = 0
+    sum_sqr = 0
+    if len(a) >= 2:
+        for i in range(len(a)):
+            sum_sqr = sum_sqr + (b[i] - a[i])**2
+        result = (sum_sqr)**0.5
+    else:
+        print("Error")
+    return result
+
+
+def opt_euclidean(a, b):
+    result = 0
+    sum_sqr = 0
+    if len(a) >= 2:
+        for i in range(len(a)):
+            sum_sqr = sum_sqr + pow((b[i] - a[i]), 2)
+        result = sqrt(sum_sqr)
+    else:
+        print("Error")
+    return result
+
+
+def np_euclidean(a, b):
+    result = 0
+    sum_sqr = 0
+    if len(a) >= 2:
+        for i in range(len(a)):
+            sum_sqr = sum_sqr + (np.power((b[i] - a[i]), 2))
+        result = np.sqrt(sum_sqr)
+    else:
+        print("Error")
+    return result
