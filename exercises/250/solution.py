902b611bcf527008c5ae284c0c01e874d2a4bc61
charlotteVDD <charlottevandend@gmail.com>
2015-09-25 09:17:57 +0200

    ex250

--- exercises/250/solution.py
+++ exercises/250/solution.py
@@ -1,32 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-pernicious number : positive interger where digit sum of its binary rep is
-prime
+Created on Fri Sep 25 08:58:34 2015
+
 @author: CharlotteVDD
 """
 
 
-def is_prime(x):
-    if x <= 0:
-        return False
-    elif x == 1:
-        return False
-    elif x == 2:
-        return True
-    elif x == 3:
-        return True
-    else:
-        for i in range(2, int(x ** 0.5) + 1):
-            if x % i == 0:
-                return False
-            else:
-                continue
-    return True
-
-
-x = range(10000, 10050)
-a = []
-
-for i in x:
-    if is_prime(i) is True:
-        print(i, end=", ")
+def draw_n_square(n):
+    for i in range(n):
+        print((n * "+---") + "+", end=None)
+        print((n + 1) * "|   ")
+    print((n * "+---") + "+", end=None)
